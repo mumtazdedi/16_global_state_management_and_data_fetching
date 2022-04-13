db9f0863d6ce05d9dae79f0416658c42db8cf720
mumtazdedi <mumtazdedi@gmail.com>
2022-04-13 13:07:12 +0700

    add build for deployment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import { React, useState } from 'react';
+import { React } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Title from './components/Title';
 import Input from './components/Input';
 import Todo from './components/Todo';
-/** redux */
-import { useDispatch, useSelector } from 'react-redux';
-import { removeTodoTask } from './store/TodoTask';
 
 
 function App() {
-
-  const todos = useSelector(state => state.todoTask);
-  const dispatch = useDispatch();
-
 
   return (
     <>
